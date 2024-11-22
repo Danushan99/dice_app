2e03ec8487d3e5bcf24eeecc296127edfce49182
Danushan <danushanr@yaratechnologies.com>
2024-11-22 08:03:27 +0530

    add widgets

---
+++ lib/widgets/quesrions_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuesrionsScreen extends StatefulWidget {
+  const QuesrionsScreen({super.key});
+
+  @override
+  State<QuesrionsScreen> createState() => _QuesrionsScreenState();
+}
+
+class _QuesrionsScreenState extends State<QuesrionsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
