d9ea455eeb8e7134a5d8e090544397dedc2453ca
Danushan <danushanr@yaratechnologies.com>
2024-11-25 08:59:26 +0530

    fetch data from data source

---
+++ lib/widgets/answer_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(
+      {required this.answerText, required this.onTap, super.key});
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+          backgroundColor: Color.fromARGB(255, 38, 1, 105),
+          foregroundColor: Colors.white),
+          
+      child: Text(answerText),
+    );
+  }
+}
--- lib/widgets/qustions_screen.dart
+++ lib/widgets/qustions_screen.dart
@@ -1,3 +1,5 @@
+import 'package:dice_app/data/questions.dart';
+import 'package:dice_app/widgets/answer_button.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -12,6 +14,25 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('QuestionsScreen');
+    final currentQuestions = questions[0];
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestions.text,
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(answerText: currentQuestions.answers[0], onTap: () {}),
+          AnswerButton(answerText: currentQuestions.answers[1], onTap: () {}),
+          AnswerButton(answerText: currentQuestions.answers[2], onTap: () {}),
+          AnswerButton(answerText: currentQuestions.answers[3], onTap: () {}),
+        ],
+      ),
+    );
   }
 }
